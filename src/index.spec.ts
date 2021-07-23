1d74551602143e38752df60d908cbb1f449781dd
TheoSabot <theo@virutalbox.com>
2021-07-23 17:28:50 +0200

    second fc

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,78 @@
-import { systemUnderTest } from ".";
+import { initGame, Game, Player, incrementScore } from "./index";
 
 describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+  it("Should initialize a game", function() {
+    const game = initGame();
+
+    const expectedGame: Game = {
+      [Player.One]: 0,
+      [Player.Two]: 0,
+    }
+
+    expect(game).toEqual(expectedGame);
+  });
+
+  it("Should increment a score from 0 to 1 for player One", function() {
+    const givenGame: Game = {
+      [Player.One]: 0,
+      [Player.Two]: 0,
+    }
+    
+    const game = incrementScore(givenGame, Player.One);
+
+    const expectedGame: Game = {
+      [Player.One]: 1,
+      [Player.Two]: 0,
+    }
+
+    expect(game).toEqual(expectedGame);
+  });
+
+  it("Should increment a score from 0 to 1 for player Two", function() {
+    const givenGame: Game = {
+      [Player.One]: 0,
+      [Player.Two]: 0,
+    }
+    
+    const game = incrementScore(givenGame, Player.Two);
+
+    const expectedGame: Game = {
+      [Player.One]: 0,
+      [Player.Two]: 1,
+    }
+
+    expect(game).toEqual(expectedGame);
+  });
+
+  it("Should increment a score from 1 to 2 for player One", function() {
+    const givenGame: Game = {
+      [Player.One]: 1,
+      [Player.Two]: 0,
+    }
+    
+    const game = incrementScore(givenGame, Player.One);
+
+    const expectedGame: Game = {
+      [Player.One]: 2,
+      [Player.Two]: 0,
+    }
+
+    expect(game).toEqual(expectedGame);
+  });
+
+  it("Should increment a score from 1 to 2 for player Two", function() {
+    const givenGame: Game = {
+      [Player.One]: 0,
+      [Player.Two]: 1,
+    }
+    
+    const game = incrementScore(givenGame, Player.Two);
+
+    const expectedGame: Game = {
+      [Player.One]: 0,
+      [Player.Two]: 2,
+    }
+
+    expect(game).toEqual(expectedGame);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function systemUnderTest(): boolean {
-  return true;
+export enum Player {
+  One,
+  Two,
+}
+
+export interface Game {
+  [Player.One]: number
+  [Player.Two]: number
+}
+
+export const initGame = (): Game => {
+  return {
+    [Player.One]: 0,
+    [Player.Two]: 0,
+  };
+}
+
+export const incrementScore = (game: Game, player: Player): Game => {
+  return {...game, [player]: game[player] + 1}
 }
